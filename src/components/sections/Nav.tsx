4a48f3bc7e6bfc51568e8f6554368a994a6f3799
Dave Mitten <davidmitten88@gmail.com>
2024-10-03 10:23:38 +0100

    feat: 🎸 HOTFIX: removed nav anchor tag

--- src/components/sections/Nav.tsx
+++ src/components/sections/Nav.tsx
@@ -18,24 +18,22 @@ const MobileMenu = ({
   return (
     <nav className="relative md:hidden px-4 sm:px-0 py-6 flex justify-between items-center">
       <div onClick={() => setIsOpen(!isOpen)}>
-        <a href="/">
-          <svg
-            className="transition-colors duration-300 hover:text-highlight hover:scale-110 text-header"
-            fill="none"
-            height="24"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            width="24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="m7 11 2-2-2-2" />
-            <path d="M11 13h4" />
-            <rect height="18" rx="2" ry="2" width="18" x="3" y="3" />
-          </svg>
-        </a>
+        <svg
+          className="transition-colors duration-300 hover:text-highlight hover:scale-110 text-header"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m7 11 2-2-2-2" />
+          <path d="M11 13h4" />
+          <rect height="18" rx="2" ry="2" width="18" x="3" y="3" />
+        </svg>
       </div>
 
       <ul
